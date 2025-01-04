746e9e8cd46a59fdbec919419b6784855a805773
zhangyemengren <624593758@qq.com>
2025-01-04 23:33:01 +0800

    update

--- recipes_swift/ContentView.swift
+++ recipes_swift/ContentView.swift
@@ -1,24 +1,70 @@
-//
-//  ContentView.swift
-//  recipes_swift
-//
-//  Created by 张弛 on 2024/12/9.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                }
+                Section{
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("How much tip do you want to leave?"){
+                    Picker("Tip pecentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format:.percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                Section {
+
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+
+                }
+            }
+            .navigationTitle("Wespilt")
+            .toolbar {
+                if amountIsFocused {
+
+                    Button("Done") {
+
+                        amountIsFocused = false
+
+                    }
+
+                }
+
+            }
         }
-        .padding()
+        
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
